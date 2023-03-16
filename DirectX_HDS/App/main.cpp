890f2e17dfce7074ad18c248f8d5043358a050c4
댕 허 <zest2700@gmail.com>
2023-03-17 00:19:46 +0900

    허동석 : 다이렉트X 2일차 업데이트

--- DirectX_HDS/App/main.cpp
+++ DirectX_HDS/App/main.cpp
@@ -1,5 +1,11 @@
 #include <Windows.h>
-#include <GameEngineBase/GameEngineSerializer.h>
+#include <GameEngineBase\GameEngineFile.h>
+#include <GameEngineBase\GameEngineDirectory.h>
+#include <GameEngineCore\GameEngineCore.h>
+
+// 라이브러리 디렉터리 설정으로 인해 lib 파일 앞쪽에 상대경로가 포함되어 있는 것이고
+// 그로 인해 아래와 같이 사용이 가능하다. 
+#pragma comment(lib, "GameEngineBase.lib")
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -9,3 +15,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	//GameEngineCore::Start(hInstance, );
 }
 
+// 3월 17일 해야할일 목록
+// 1. 코드 읽으면서 주석 다시 하나하나 달면서 체크 
+// 2. 파일입출력, Rtti , 한번씩 확인해보면서 공부.
