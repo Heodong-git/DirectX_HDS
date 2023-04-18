b63119d5e5d207f3917be96a3dcc86eed8a7c273
댕 허 <zest2700@gmail.com>
2023-04-18 17:04:30 +0900

    추가

--- DirectX_HDS/GameEngineCore/GameEngineShaderResHelper.cpp
+++ DirectX_HDS/GameEngineCore/GameEngineShaderResHelper.cpp
@@ -1,14 +1,11 @@
 #include "PrecompileHeader.h"
 #include "GameEngineShaderResHelper.h"
 
-
+// 
 void GameEngineShaderResHelper::Copy(const GameEngineShaderResHelper& _ResHelper)
 {
-
 	for (const std::pair<std::string, GameEngineConstantBufferSetter>& Setter : _ResHelper.ConstantBuffer)
 	{
 		ConstantBuffer.insert(Setter);
 	}
-
-
 }
