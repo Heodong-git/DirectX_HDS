a7df8feedc3af80fe6ea906d1507762f3965dbb1
댕 허 <zest2700@gmail.com>
2023-05-14 11:57:15 +0900

    플레이어 애니메이션 추가 완료

--- DirectX_HDS/GameEngineContents/Player.cpp
+++ DirectX_HDS/GameEngineContents/Player.cpp
@@ -27,28 +27,6 @@ Player::~Player()
 
 void Player::Start()
 {
-	// 파일로드 기본인터페이스 
-	{
-		if (nullptr == GameEngineSprite::Find("player_idle"))
-		{
-			GameEngineDirectory Dir;
-			Dir.MoveParentToDirectory("katanazero_resources");
-			Dir.Move("katanazero_resources");
-			Dir.Move("Texture");
-			Dir.Move("ClubLevel");
-			Dir.Move("player");
-
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_idle").GetFullPath());
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_attack").GetFullPath());
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_idle_to_run").GetFullPath());
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_run").GetFullPath());
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_crouch").GetFullPath());
-			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_flip").GetFullPath());
-
-			std::vector<GameEngineFile> File = Dir.GetAllFile({ ".Png", });
-		}
-	}
-	
 	if (false == GameEngineInput::IsKey("player_slash"))
 	{
 		GameEngineInput::CreateKey("player_DebugSwitch", 'Q');
@@ -99,6 +77,39 @@ void Player::Render(float _DeltaTime)
 
 void Player::CreateAnimation()
 {
+	// 파일로드 기본인터페이스 
+	{
+		if (nullptr == GameEngineSprite::Find("player_idle"))
+		{
+			GameEngineDirectory Dir;
+			Dir.MoveParentToDirectory("katanazero_resources");
+			Dir.Move("katanazero_resources");
+			Dir.Move("Texture");
+			Dir.Move("ClubLevel");
+			Dir.Move("player");
+
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_idle").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_attack").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_idle_to_run").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_run").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_run_to_idle").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_crouch").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_flip").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_doorbreak").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_doorbreak_full").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_dooropen_gentle").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_fall").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_hurtfly_begin").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_hurtfly_loop").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_hurtground").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_hurtrecover").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_jump").GetFullPath());
+			GameEngineSprite::LoadFolder(Dir.GetPlusFileName("player_wallslide").GetFullPath());
+
+			std::vector<GameEngineFile> File = Dir.GetAllFile({ ".Png", });
+		}
+	}
+
 	m_Render->CreateAnimation({ .AnimationName = "player_idle", .SpriteName = "player_idle", .Start = 2, .End = 10 ,
 									  .FrameInter = 0.12f , .Loop = true , .ScaleToTexture = true});
 
@@ -111,11 +122,44 @@ void Player::CreateAnimation()
 	m_Render->CreateAnimation({ .AnimationName = "player_run", .SpriteName = "player_run", .Start = 0, .End = 9 ,
 								  .FrameInter = 0.05f , .Loop = true , .ScaleToTexture = true });
 
+	m_Render->CreateAnimation({ .AnimationName = "player_run_to_idle", .SpriteName = "player_run_to_idle", .Start = 0, .End = 4 ,
+								  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
 	m_Render->CreateAnimation({ .AnimationName = "player_crouch", .SpriteName = "player_crouch", .Start = 0, .End = 0 ,
 							  .FrameInter = 0.01f , .Loop = false , .ScaleToTexture = true });
 
 	m_Render->CreateAnimation({ .AnimationName = "player_flip", .SpriteName = "player_flip", .Start = 0, .End = 10,
 						  .FrameInter = 0.03f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_doorbreak", .SpriteName = "player_doorbreak", .Start = 0, .End = 5,
+						  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_doorbreak_full", .SpriteName = "player_doorbreak_full", .Start = 0, .End = 9,
+						  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_dooropen_gentle", .SpriteName = "player_dooropen_gentle", .Start = 0, .End = 8,
+						  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_fall", .SpriteName = "player_fall", .Start = 0, .End = 3,
+						  .FrameInter = 0.05f , .Loop = true , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_hurtfly_begin", .SpriteName = "player_hurtfly_begin", .Start = 0, .End = 1,
+						  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_hurtfly_loop", .SpriteName = "player_hurtfly_loop", .Start = 0, .End = 3,
+						  .FrameInter = 0.05f , .Loop = true , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_hurtground", .SpriteName = "player_hurtground", .Start = 0, .End = 5,
+					  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_hurtrecover", .SpriteName = "player_hurtrecover", .Start = 0, .End = 8,
+					  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_jump", .SpriteName = "player_jump", .Start = 0, .End = 3,
+					  .FrameInter = 0.05f , .Loop = true , .ScaleToTexture = true });
+
+	m_Render->CreateAnimation({ .AnimationName = "player_wallslide", .SpriteName = "player_wallslide", .Start = 0, .End = 1,
+					  .FrameInter = 0.05f , .Loop = false , .ScaleToTexture = true });
 
 	m_Render->ChangeAnimation("player_idle");
 }
